3a63d75a4b8402a7eeb07291c42316907e2135cf
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-06-05 22:01:48 -0400

    6/5

---
+++ app/components/Note.js
@@ -0,0 +1,25 @@
+var React = require('react');
+
+var ReactDOM = require('react-dom');
+
+var Note = React.createClass({
+	edit: function() {
+		alert('editing note');
+	},
+	remove: function() {
+		alert('removing note');
+	},
+	render: function(){
+		return(
+			<div className="note">
+			<p>{this.props.children}</p>
+			<span>
+				<button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil" />
+				<button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash" />
+			</span>
+			</div>
+			);
+	}
+});
+
+ReactDOM.render(<Note>Hello World</Note>, document.getElementById('react-container'));
